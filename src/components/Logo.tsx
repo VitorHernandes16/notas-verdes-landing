eca4a2ad6aa341289817c7587d057791aade534e
VitorHernandes16 <167880667+VitorHernandes16@users.noreply.github.com>
2025-05-09 09:24:49 -0300

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,9 +5,8 @@ const Logo = () => {
   return (
     <div className="flex items-center space-x-2">
       <div className="w-8 h-8 rounded-full bg-primary flex items-center justify-center">
-        <span className="text-white font-bold text-lg">S</span>
+       <img src="logo.png" alt="Sasbemc" />
       </div>
-      <span className="text-lg font-bold text-primary">Sasbemc</span>
     </div>
   );
 };
