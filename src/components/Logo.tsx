cdf0eabe9ec54cb99aa3259aec3ef41596aa3c9a
VitorHernandes16 <167880667+VitorHernandes16@users.noreply.github.com>
2025-05-14 08:15:38 -0300

    tamanho_logo2

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,11 +3,9 @@ import React from 'react';
 
 const Logo = () => {
   return (
-    <div className="flex items-center space-x-2">
       <div className="w-8 h-8 rounded-full bg-primary flex items-center justify-center">
        <img src="logo.png" alt="Sasbemc" className="w-full h-full object-contain" />
       </div>
-    </div>
   );
 };
 
