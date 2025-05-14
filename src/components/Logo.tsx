1f877b1f51bf765f2a9ad928b3a0c7e1a0beefc3
VitorHernandes16 <167880667+VitorHernandes16@users.noreply.github.com>
2025-05-14 07:59:29 -0300

    logo2

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,7 +5,7 @@ const Logo = () => {
   return (
     <div className="flex items-center space-x-2">
       <div className="w-8 h-8 rounded-full bg-primary flex items-center justify-center">
-       <img src="logo.png" alt="Sasbemc" />
+       <img src="logo.png" alt="Sasbemc" className="w-full h-full object-contain" />
       </div>
     </div>
   );
